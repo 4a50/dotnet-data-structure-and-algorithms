a6f90f9f2b8f4fa9080d830e09a6711bcd7f4ade
4a50 <jpjones22@gmail.com>
2021-01-22 15:56:17 -0800

    Stack class created

--- DataStructures/Implementations/Program.cs
+++ DataStructures/Implementations/Program.cs
@@ -1,18 +1,34 @@
-﻿namespace DataStructures
+﻿using System;
+namespace DataStructures
 {
     class Program
     {
         static void Main(string[] args)
         {
-            LinkedList myList = new LinkedList(3);
-            myList.Insert(2);
-            myList.Insert(22);
-            myList.Insert(81);
-            myList.Insert(1999);
-            myList.Insert(741);
-            myList.InsertBefore(22, 111);
-            myList.Print();
-            myList.PrintR(myList.Head);
+            //LinkedList myList = new LinkedList(3);
+            //myList.Insert(2);
+            //myList.Insert(22);
+            //myList.Insert(81);
+            //myList.Insert(1999);
+            //myList.Insert(741);
+            //myList.InsertBefore(22, 111);
+            //myList.Print();
+            //myList.PrintR(myList.Head);
+
+            Stack stack = new Stack();
+
+            stack.Push(19);
+            stack.Push(18);
+            stack.Push(17);
+            stack.Push(16);
+            Console.WriteLine($"TOP: {stack.Top.Value}");
+            stack.Pop();
+            stack.Pop();
+            stack.Pop();
+            Console.WriteLine($"TOP: {stack.Top.Value}");
+
+
+
         }
     }
 }
---
+++ DataStructures/Implementations/Stack.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures
+{
+    class Stack
+    {
+        public Node Top { get; set; }
+
+        public void Push(int value)
+        {
+            Node newNode = new Node(value);
+            newNode.Next = Top;
+            Top = newNode;            
+        }
+        public void Pop()
+        {
+            Node tempNode = Top.Next;
+            Top = tempNode;            
+        }
+        
+        public int Peek()
+        {
+            return Top.Value;
+        }
+
+        public bool isEmpty()
+        {
+            if (Top != null) return false;
+            return true;
+        }
+    }
+}
