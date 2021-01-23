493f4ae955fb3a61676026e7a55ebc9086bd36d2
4a50 <jpjones22@gmail.com>
2021-01-22 17:16:41 -0800

    Complete Writing Stack Tests.

--- DataStructures/DataStructuresTest/StackTest.cs
+++ DataStructures/DataStructuresTest/StackTest.cs
@@ -1,16 +1,92 @@
 ﻿using DataStructures;
+using System;
 using Xunit;
 namespace DataStructuresTest
 {
     public class StackTest
     {
         /// <summary>
-        /// Success fully push a node onto a stack
+        /// Successfully pushes a node onto a stack
         /// </summary>
-        public void Push_On_A_Stack()
+        [Fact]
+        public void Push_Node_Onto_A_Stack()
         {
-            StackTest 
+            Stack stack = new Stack();
+            stack.Push(5);            
+            Assert.Equal(5, stack.Peek());
+        }
+        /// <summary>
+        /// Successfully pushes multiple Nodes onto the Stack.
+        /// </summary>
+        [Fact]
+        public void Push_Multiple_Nodes_From_A_Stack()
+        {
+            Stack stack = new Stack();
+            stack.Push(1);
+            stack.Push(3);
+            stack.Push(5);
+            stack.Push(7);
+            Assert.Equal(7, stack.Peek());
+        }
+        /// <summary>
+        /// Successfully removes a Node from the Top of the Stack
+        /// </summary>
+        [Fact]
+        public void Remove_Node_From_Stack()
+        {
+            Stack stack = new Stack();
+            stack.Push(1);
+            stack.Push(3);
+            stack.Push(5);
+            stack.Push(7);
+            stack.Pop();
+            Assert.Equal(5, stack.Peek());
 
+        }
+        /// <summary>
+        /// Remove all nodes from a stack
+        /// </summary>
+        [Fact]
+        public void Pop_All_Nodes_From_A_Stack()
+        {
+            Stack stack = new Stack();
+            stack.Push(1);
+            stack.Push(3);
+            stack.Push(5);
+            stack.Push(7);
+            stack.Pop();
+            stack.Pop();
+            stack.Pop();
+            stack.Pop();
+            Assert.True(stack.isEmpty());
+        }
+        /// <summary>
+        /// Return expected value for node on top of stack
+        /// </summary>
+        [Fact]
+        public void Peek_At_Node_On_Top_Of_Stack()
+        {
+            Stack stack = new Stack();
+            stack.Push(1);
+            stack.Push(3);
+            stack.Push(5);
+            stack.Push(7);
+            Assert.Equal(7, stack.Peek());
+        }
+        /// <summary>
+        /// Create an empty stack that does not contain any nodes.
+        /// </summary>
+        [Fact]
+        public void Instantiate_An_Empty_Stack()
+        {
+            Stack stack = new Stack();            
+            Assert.True(stack.isEmpty());
+        }
+        [Fact]
+        public void Using_Pop_On_An_Empty_Stack_Raises_An_Excpetion()
+        {
+            Stack stack = new Stack();
+            Assert.Throws<NullReferenceException>(stack.Pop);
 
         }
     }
