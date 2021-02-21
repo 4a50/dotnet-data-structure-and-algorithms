603b1c6356e19eb5b7b78de471a392067fc04642
4a50 <jpjones22@gmail.com>
2021-02-21 12:21:15 -0800

    Adjusting LinkedList Testing for T Generics

--- DataStructures/DataStructuresTest/LinkedListTests.cs
+++ DataStructures/DataStructuresTest/LinkedListTests.cs
@@ -1,4 +1,5 @@
 using DataStructures;
+using System;
 using Xunit;
 namespace DataStructuresTest
 {
@@ -9,7 +10,7 @@ namespace DataStructuresTest
     {
       int value = 2;
 
-      LinkedList testList = new LinkedList(value);
+      LinkedList<int> testList = new LinkedList<int>(value);
 
       Assert.Equal(testList.Head.Value, value);
 
@@ -18,7 +19,7 @@ namespace DataStructuresTest
     [Fact]
     public void After_Insert_New_Node_Is_Head()
     {
-      LinkedList testList = new LinkedList(1);
+      LinkedList<int> testList = new LinkedList<int>(1);
       // H(2) -> (1)
       testList.Insert(2);
 
@@ -29,7 +30,7 @@ namespace DataStructuresTest
     public void After_Insert_New_Head_Next_is_Old_Head()
     {
       // H(1)
-      LinkedList testList = new LinkedList(1);
+      LinkedList<int> testList = new LinkedList<int>(1);
       Node<int> oldHead = testList.Head;
 
       // H(2) -> (1)
@@ -41,7 +42,7 @@ namespace DataStructuresTest
     [Fact]
     public void Find_Value_In_The_Linked_List()
     {
-      LinkedList testList = new LinkedList(4);
+      LinkedList<int> testList = new LinkedList<int>(4);
       testList.Insert(23);
       Assert.True(testList.Includes(23));
 
@@ -49,7 +50,7 @@ namespace DataStructuresTest
     [Fact]
     public void Does_Not_Find_Value_In_Linked_List()
     {
-      LinkedList testList = new LinkedList(4);
+      LinkedList<int> testList = new LinkedList<int>(4);
       testList.Insert(23);
       testList.Insert(500);
       Assert.False(testList.Includes(66));
@@ -57,7 +58,7 @@ namespace DataStructuresTest
     [Fact]
     public void Properly_Insert_Multiple_Nodes()
     {
-      LinkedList testList = new LinkedList(4);
+      LinkedList<int> testList = new LinkedList<int>(4);
       testList.Insert(2);
       testList.Insert(22);
       testList.Insert(81);
@@ -67,7 +68,7 @@ namespace DataStructuresTest
     [Fact]
     public void Properly_Returns_All_Values_In_List()
     {
-      LinkedList testList = new LinkedList(34);
+      LinkedList<int> testList = new LinkedList<int>(34);
       string testStr = "[490] => [162] => [34] => Null";
       testList.Insert(162);
       testList.Insert(490);
@@ -76,13 +77,13 @@ namespace DataStructuresTest
     [Fact]
     public void Create_An_Empty_List()
     {
-      LinkedList testList = new LinkedList();
+      LinkedList<int> testList = new LinkedList<int>();
       Assert.Null(testList.Head);
     }
     [Fact]
     public void Append_A_Value_To_Linked_List()
     {
-      LinkedList testList = new LinkedList(12);
+      LinkedList<int> testList = new LinkedList<int>(12);
       testList.Insert(2);
       testList.Insert(22);
       testList.Insert(81);
@@ -93,7 +94,7 @@ namespace DataStructuresTest
     [Fact]
     public void Insert_New_Value_After_Requested_Value()
     {
-      LinkedList testList = new LinkedList();
+      LinkedList<int> testList = new LinkedList<int>();
       testList.Insert(2);
       testList.Insert(22);
       testList.Insert(81);
@@ -105,18 +106,18 @@ namespace DataStructuresTest
     [Fact]
     public void Insert_New_Value_After_Value_Not_Found()
     {
-      LinkedList testList = new LinkedList();
+      LinkedList<int> testList = new LinkedList<int>();
       testList.Insert(2);
       testList.Insert(22);
       testList.Insert(81);
       testList.Insert(1999);
       testList.Insert(741);
-      Assert.Equal(-1, testList.InsertAfter(123, 705));
+      Assert.Equal(0, testList.InsertAfter(123, 705));
     }
     [Fact]
     public void Insert_New_Value_Before_Requested_Value()
     {
-      LinkedList testList = new LinkedList();
+      LinkedList<int> testList = new LinkedList<int>();
       testList.Insert(2);
       testList.Insert(22);
       testList.Insert(81);
@@ -127,13 +128,13 @@ namespace DataStructuresTest
     [Fact]
     public void Insert_New_Value_Before_Requested_Value_Not_Found()
     {
-      LinkedList testList = new LinkedList();
+      LinkedList<int> testList = new LinkedList<int>();
       testList.Insert(2);
       testList.Insert(22);
       testList.Insert(81);
       testList.Insert(1999);
-      testList.Insert(741);
-      Assert.Equal(-1, testList.InsertBefore(100, 199));
+      testList.Insert(741);      
+      Assert.Equal(0,testList.InsertBefore(100, 199));
     }
     /// <summary>
     /// These tests the Kth value from the Tail in a linked List
@@ -141,18 +142,20 @@ namespace DataStructuresTest
     [Fact]
     public void Kth_Node_Location_Of_Node_Is_Beyond_The_Range_Of_The_List()
     {
-      LinkedList testList = new LinkedList();
+      LinkedList<int> testList = new LinkedList<int>();
       testList.Insert(12);
       testList.Insert(24);
       testList.Insert(48);
       testList.Insert(60);
       testList.Insert(72);
-      Assert.Equal(-2, testList.LocateKthNodeFromTail(6));
+      var except = Assert.Throws<Exception>(() => testList.LocateKthNodeFromTail(6));
+
+      Assert.Equal("K is outside the length of the list", except.Message);
     }
     [Fact]
     public void Kth_Value_And_Linked_List_Length_Are_Equal()
     {
-      LinkedList testList = new LinkedList();
+      LinkedList<int> testList = new LinkedList<int>();
       testList.Insert(12);
       testList.Insert(24);
       testList.Insert(48);
@@ -163,25 +166,27 @@ namespace DataStructuresTest
     [Fact]
     public void K_Value_Is_A_Negative_Number()
     {
-      LinkedList testList = new LinkedList();
+      LinkedList<int> testList = new LinkedList<int>();
       testList.Insert(12);
       testList.Insert(24);
       testList.Insert(48);
       testList.Insert(60);
       testList.Insert(72);
-      Assert.Equal(-1, testList.LocateKthNodeFromTail(-1));
+      var except = Assert.Throws<Exception>(() => testList.LocateKthNodeFromTail(-1));
+
+      Assert.Equal("K is less than 0", except.Message);
     }
     [Fact]
     public void Kth_Node_In_A_Linked_List_Of_One()
     {
-      LinkedList testList = new LinkedList();
+      LinkedList<int> testList = new LinkedList<int>();
       testList.Insert(12);
       Assert.Equal(12, testList.LocateKthNodeFromTail(0));
     }
     [Fact]
     public void Find_Kth_Value_In_A_Linked_List()
     {
-      LinkedList testList = new LinkedList();
+      LinkedList<int> testList = new LinkedList<int>();
       testList.Insert(12);
       testList.Insert(24);
       testList.Insert(48);
