94c8d3679c2291b800e21175ad7c7272ac5fdd94
4a50 <jpjones22@gmail.com>
2021-02-28 12:11:21 -0800

    Added ability to normalize the input string

--- RepeatedWord/RepeatedWord.cs
+++ RepeatedWord/RepeatedWord.cs
@@ -12,26 +12,30 @@ namespace DataStructures
     }
 //TODO: Understand that 'was' and 'time' hash out same.  Perhaps institute a KVP LL
     
-    public string FindRepeatedWord(string phrase)
+    public string FindRepeatedWord(string phrase, bool normalize = true)
     {      
+
       int hashWord = 0;
-      string word = "";
+      string returnWord = "";
+      string phraseWord = "";
       string[] parsedPhrase = Regex.Split(phrase, @"\W+");
       for (int i = 0; i < parsedPhrase.Length; i++)
-      {        
-        hashWord = Map.Hash(parsedPhrase[i]);
-        if (Map.Contains(parsedPhrase[i]))
+      {
+        if (normalize) { phraseWord = parsedPhrase[i].ToUpper(); }
+        else { phraseWord = parsedPhrase[i]; }
+        hashWord = Map.Hash(phraseWord);
+        if (Map.Contains(phraseWord))
         {
-          word = parsedPhrase[i];
-          return word;
+          returnWord = phraseWord;
+          return returnWord;
         }
         else
         {
-          Map.Add(parsedPhrase[i], "value");
+          Map.Add(phraseWord, "value");
         }
       }
       Console.WriteLine();
-      return word;
+      return returnWord;
       //TODO: Figure out how to determine the duplicate entry.  Regex OK
       
     }
